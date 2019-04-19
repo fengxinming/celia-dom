9b88f56a38260bb7debd7dc94ca3915209999727
奉新明 <fxm0016@gmail.com>
2019-04-19 15:10:09 +0800

    fix(checkdom): 修复检查dom和类数组的一个bug

--- src/_internal/_checkDom.js
+++ src/_internal/_checkDom.js
@@ -1,16 +1,16 @@
 import forEach from 'celia/_internal/_array/_forEach';
 import forSlice from 'celia/_internal/_array/_forSlice';
 import isWindow from 'celia/isWindow';
+import isArrayLike from 'celia/isArrayLike';
 
 export function firstNode(dom) {
   return dom && (dom.length > -1 ? dom[0] : dom);
 }
 
 export function manip(dom, mcallback, callback) {
-  const len = dom.length;
   let elem, last;
-  if (len) {
-    forSlice(dom, 0, (last = len - 1), mcallback);
+  if (isArrayLike(dom)) {
+    forSlice(dom, 0, (last = dom.length - 1), mcallback);
     elem = dom[last];
   } else {
     elem = dom;
@@ -21,7 +21,7 @@ export function manip(dom, mcallback, callback) {
 
 export default function (dom, callback) {
   if (dom) {
-    if (dom.length && !isWindow(dom)) {
+    if (isArrayLike(dom) && !isWindow(dom)) {
       forEach(dom, callback);
     } else {
       callback(dom);
